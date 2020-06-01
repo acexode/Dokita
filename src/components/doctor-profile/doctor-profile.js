32246105b9eb7aaf91dc27feed1761be88e0d42e
Acexode <abubakar.abdulwahab@outsourceglobal.com>
2020-06-01 23:03:36 +0100

    fixed env

--- src/components/doctor-profile/doctor-profile.js
+++ src/components/doctor-profile/doctor-profile.js
@@ -2,6 +2,7 @@ import React from 'react'
 import  './doctor-profile.css'
 import WrappedMap from '../Map/GoogleMap'
 // import { key } from '../../key';
+import {} from 'dotenv/config'
 import Book from '../book/book'
 import { withRouter } from 'react-router-dom'
 const DoctorProfile = ({location}) => {
