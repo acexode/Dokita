e6d53b768ef557377875c7fd164e835ef5fe652c
Acexode <abubakar.abdulwahab@outsourceglobal.com>
2020-06-03 17:14:46 +0100

    added redux

--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -1,9 +1,27 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-import {firebaseConfig} from '../../firebase.config'
-
-firebase.initializeApp(firebaseConfig)
+// import devConfig from '../../firebase.config'
+export const prodConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+  };
+//   console.log(process.env.NODE_ENV === "production")
+//   let config 
+//   if(process.env.NODE_ENV === "production"){
+//         config = prodConfig
+//   }else{
+//       config = devConfig
+//   }
+//   console.log(config)
+firebase.initializeApp(prodConfig)
+// firebase.initializeApp(firebaseConfig)
 
 export const firestore = firebase.firestore()
 
